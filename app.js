94863cfa599e4c30379464f3eec59f557409bc7b
NaveenKumar <naveenkumar12062002@gmail.com>
2021-06-05 11:36:59 +0530

    Portfolio Hosting

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const app = express();
+const port = 8000;
+
+
+app.use('/static', express.static('static')) // For serving static files
+express.urlencoded({extended: true})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+ 
+
+
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    res.status(200).render('index.pug');
+})
+
+
+
+
+
+
+
+
+
+
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
